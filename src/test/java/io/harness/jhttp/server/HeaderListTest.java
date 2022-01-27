bd3a714f06b1ab72655e36aeefed76684619f0ed
relliott-harness <78382621+relliott-harness@users.noreply.github.com>
2022-01-27 09:14:56 -0600

    Update HeaderListTest.java

--- src/test/java/io/harness/jhttp/server/HeaderListTest.java
+++ src/test/java/io/harness/jhttp/server/HeaderListTest.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.junit.Test;
 
+import java.util.concurrent;
+
 import static org.junit.Assert.*;
 
 public class HeaderListTest {
@@ -14,7 +16,7 @@ public class HeaderListTest {
         final HeaderList headerList = new HeaderList();
         headerList.addHeader("h1", "v1");
         headerList.addHeader("h2", "v2");
-
+        TimeUnit.SECONDS.sleep(2);
         assertEquals(2, headerList.getHeaders().size());
         assertEquals("v1", headerList.getHeader("h1"));
         assertEquals("v2", headerList.getHeader("h2"));
@@ -26,7 +28,7 @@ public class HeaderListTest {
         headerList.addHeader("h1", "v1");
         headerList.addHeader("h2", "v2");
         headerList.setHeader("h1", "xyz");
-
+        TimeUnit.SECONDS.sleep(2);
         assertEquals(2, headerList.getHeaders().size());
         assertEquals("xyz", headerList.getHeader("h1"));
         assertEquals("v2", headerList.getHeader("h2"));
@@ -38,7 +40,7 @@ public class HeaderListTest {
         headerList.addHeader("h1", "v1");
         headerList.addHeader("h1", "v2");
         headerList.setHeader("h1", "xyz");
-
+        TimeUnit.SECONDS.sleep(2);
         assertEquals(2, headerList.getHeaders().size());
         final List<String> headers = headerList.getHeaders("h1");
         assertEquals("xyz", headers.get(0));
@@ -51,7 +53,7 @@ public class HeaderListTest {
         headerList.addHeader("h1", "v1");
         headerList.addHeader("h1", "v2");
         headerList.addHeader("h2", "v3");
-
+        TimeUnit.SECONDS.sleep(2);
         assertEquals(3, headerList.getHeaders().size());
         final List<String> headers = headerList.getHeaders("h1");
         assertEquals("v1", headers.get(0));
